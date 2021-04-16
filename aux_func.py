dbcc99df15965113fe6bd6a11566f6457cfcbbf6
gibsonxue <yx20778@essex.ac.uk>
2021-04-16 21:37:19 +0100

    the first commmit

--- aux_func.py
+++ aux_func.py
@@ -1,41 +1,5 @@
-import os,pathlib,re,nltk,pickle,string
-nltk.download('punkt')
-nltk.download('stopwords')
-from nltk.tokenize import word_tokenize
-from collections import Counter
-from nltk.stem import WordNetLemmatizer
-from sklearn.metrics import f1_score
+import re
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_datasets as tfds
-import tensorflow_text as text
-from tensorflow.keras import Input, layers, losses, preprocessing, utils
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from keras.layers.core import Dense,Dropout
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-# ML Libraries
-from sklearn.metrics import accuracy_score,confusion_matrix
-from zhon import hanzi
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-import csv
-import tokenization
-from focal_loss import BinaryFocalLoss
-from sklearn.metrics import f1_score
-from imblearn.under_sampling import RandomUnderSampler
-from transformers import TFDistilBertModel,DistilBertTokenizer
-try:
-    %tensorflow_version 2.x
-except Exception:
-    pass
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 def imbalance_under_sampling(dfname):
   df_label_a = dfname[dfname['label'] == 1]
